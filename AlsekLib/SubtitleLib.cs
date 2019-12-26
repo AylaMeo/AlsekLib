ae0c00b615eac22a73ce297217da458115dc7561
Alsekwolf <etpayla@outlook.com>
2019-12-25 22:31:14 -0500

    Added SubtitleLib Class

---
+++ AlsekLib/SubtitleLib.cs
@@ -0,0 +1,77 @@
+using static CitizenFX.Core.Native.API;
+
+namespace AlsekLib
+{
+    #region Custom Subtitle class
+    /// <summary>
+    /// Custom Subtitle class used to display subtitles using preformatted templates.
+    /// Optionally you can also use a blank/custom style if you don't want to use an existing template.
+    /// </summary>
+    public static class SubtitleLib
+    {
+        /// <summary>
+        /// Custom (white/custom text style subtitle)
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        public static void Custom(string message, int duration = 2500, bool drawImmediately = true)
+        {
+            BeginTextCommandPrint("CELL_EMAIL_BCON"); // 10x ~a~
+            foreach (string s in CitizenFX.Core.UI.Screen.StringToArray(message))
+            {
+                AddTextComponentSubstringPlayerName(s);
+            }
+            EndTextCommandPrint(duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Alert (yellow text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public static void Alert(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~y~" + prefix + " ~s~" : "~y~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Error (red text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public static void Error(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~r~" + prefix + " ~s~" : "~r~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Info (blue text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public static void Info(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~b~" + prefix + " ~s~" : "~b~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Success (green text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public static void Success(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~g~" + prefix + " ~s~" : "~g~") + message, duration, drawImmediately);
+        }
+    }
+    #endregion
+}
